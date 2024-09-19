80065fd0fcc809f33f48b7159b37fa791fab81c0
Abhinav295 <Abhinav295@gmail.com>
2024-09-20 02:53:08 +0530

    Added Structure

---
+++ SheetalCity/SheetalCity/src/main/java/com/SheetalCity/SheetalCity/SheetalCityApplication.java
@@ -0,0 +1,14 @@
+package com.SheetalCity.SheetalCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetalCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetalCityApplication.class, args);
+		System.out.println("bhin");
+	}
+
+}
---
+++ SheetalCity/SheetalCity/src/test/java/com/SheetalCity/SheetalCity/SheetalCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SheetalCity.SheetalCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetalCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
