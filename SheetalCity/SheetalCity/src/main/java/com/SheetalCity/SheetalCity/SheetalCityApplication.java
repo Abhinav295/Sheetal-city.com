2586d12d12cc93d736335bd41e352d8395d831d4
Abhinav295 <Abhinav295@gmail.com>
2024-09-20 03:14:14 +0530

    eurek svr

--- SheetalCity/SheetalCity/src/main/java/com/SheetalCity/SheetalCity/SheetalCityApplication.java
+++ SheetalCity/SheetalCity/src/main/java/com/SheetalCity/SheetalCity/SheetalCityApplication.java
@@ -1,9 +1,10 @@
 package com.SheetalCity.SheetalCity;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SheetalCityApplication {
 
 	public static void main(String[] args) {
